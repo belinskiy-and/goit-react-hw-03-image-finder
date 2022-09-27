4abced11b8eab821f6c21120d8d62b0ba2305644
belinskiy-and <90721066+belinskiy-and@users.noreply.github.com>
2022-09-27 22:11:47 +0300

    added Searchbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { GlobalStyle } from "./GlobalStyle";
+import Searchbar from "./Searchbar";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Searchbar />
+      <GlobalStyle />
+    </>
   );
 };
